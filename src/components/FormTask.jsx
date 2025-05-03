8a93ff3998ccae992cb7cdfe6b4cf1e54ad46dcb
lfbernalU <luis.bernal@galileo.edu>
2025-05-03 09:30:56 -0600

    feat: integrate Redux for state management and add todo functionality

--- src/components/FormTask.jsx
+++ src/components/FormTask.jsx
@@ -2,24 +2,51 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Stack } from 'react-bootstrap';
 
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../store/todoSlice';
+
 function FormTask() {
+
+  const dispatch = useDispatch();
+  const inputRefName = useRef();
+ const inputRefDescription = useRef();
+ const inputRefDueDate = useRef();
+
+  const addItem = (e) => {
+    e.preventDefault();
+
+    if (inputRefName.current.value === '' || inputRefDescription.current.value === '' || inputRefDueDate.current.value === '') {
+      alert('Please fill all fields')
+      return;
+    }
+
+    const newItem = {
+      name: inputRefName.current.value,
+      description: inputRefDescription.current.value,
+      dueDate: inputRefDueDate.current.value
+    }
+    
+    dispatch(addTodo(newItem))
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Nombre</Form.Label>
-        <Form.Control type="text" />
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" ref={inputRefName} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicDescription">
-        <Form.Label>Descripción</Form.Label>
-        <Form.Control as="textarea" type="text" rows={4}/>
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" type="text" rows={4} ref={inputRefDescription}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicDate">
-        <Form.Label>Fecha Vencimiento</Form.Label>
-        <Form.Control type="date"/>
+        <Form.Label>Due Date</Form.Label>
+        <Form.Control type="date" ref={inputRefDueDate}/>
       </Form.Group>
-      <Stack className='d-flex justify-content-center' direction='horizontal'>
-        <Button variant="primary" type="submit">
-            ADD GOAL
+      <Stack className='d-flex justify-content-center' direction='horizontal' >
+        <Button variant="primary" onClick={addItem}>
+            Add Goal
         </Button>
       </Stack>
     </Form>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.jsx'
+import store from './store.js'
+import { Provider } from 'react-redux'
 
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
 )
